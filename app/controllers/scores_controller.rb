fa69f79acdca3392d8b1eed31d4b042b340cae84
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-11-23 22:01:14 +0900

    スコア保存機能とパラメーターの設定

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,4 +1,23 @@
 class ScoresController < ApplicationController
   def index
+    @hobby = Hobby.find(params[:hobby_id])
+    @score = Score.new
+  end
+
+  def create
+    @hobby = Hobby.find(params[:hobby_id])
+    @score = @hobby.scores.new(score_params)
+    
+    if @score.save
+      redirect_to hobby_scores_path(@hobby)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def score_params
+    params.require(:score).permit(:your_score, :rival_score).merge(user_id: current_user.id)
   end
 end
